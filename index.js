aae9293e1945a7ade380c781c0b198b35e314bad
Bruce Paul <brucewpaul@gmail.com>
2017-01-23 15:19:19 -0800

    implement checks on if the input is a number and if the square was taken

--- index.js
+++ index.js
@@ -24,28 +24,52 @@ var Game = function(player1, player2) {
     ['1','2','3'],
     ['4','5','6'],
     ['7','8','9'],
-    `player ${this.turn.name}s turn, enter the number of the square you wish to (you are ${this.turn.marker})`
+    '                                                                                '
   ];
 
-  this.playTurn = function() {
-    console.log(this.board);
+  this.playTurn = function(sameTurn) {
+    if ( sameTurn === undefined ) {
+      console.log(this.board)
+    }
+    console.log(`player ${this.turn.name}s turn, enter the number of the square you wish to select (you are ${this.turn.marker})`);
 
     rl.question('Please select a number that is still available: ', (answer) => {
 
-      answer = parseInt(answer)
-      if ( answer < 1 || answer > 9 ) {
-        this.playTurn();
+      answer = Number(answer)
+
+      if ( answer < 1 || answer > 9 || !Number.isInteger(answer) ) {
+
+        console.log(' ')
+        console.log('WARNING: you must enter a number that is on the board')
+        console.log(' ')
+
+        this.playTurn(true);
+
       } else {
 
         if ( answer <= 3 ) {
           row = this.board[0];
         } else if ( answer <= 6 ) {
           row = this.board[1];
+          answer -= 3
         } else if ( answer <= 9 ) {
           row = this.board[2];
+          answer -= 6
         }
 
-        row[answer - 1] = this.turn.marker
+        if ( row[answer - 1] === 'O' || row[answer - 1] === 'X') {
+          console.log(' ')
+          console.log('WARNING: you must enter a square that has not been taken')
+          console.log(' ')
+          return this.playTurn(true);
+        } else {
+          if ( answer % 3 === 0 ) {
+            row[answer - 1] = this.turn.marker
+          } else {
+            row[(answer % 3) - 1] = this.turn.marker
+          }
+
+        }
 
         if ( this.turn.marker === 'O' ) {
           this.turn = this.players[0]
